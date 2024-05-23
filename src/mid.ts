59cc037840793366ec71b6341dcd76b5f3599606
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-05-22 19:44:03 -0600

    refactor: validaciones adn more

--- src/mid.ts
+++ src/mid.ts
@@ -1,27 +1,49 @@
+import chalk from "chalk";
 import { charactersAll, charactersLetters, charactersNumbers } from "./data";
 import { Options } from "./models";
 
 /**
  * Generates an ID with the selected prefix from a predefined list.
  * @param {Options} [options] - The options for generating the ID.
- * @returns {string} The generated ID. The length of the generated ID will be equal to the specified length.
+ * @param {string | null} [options.prefix=null] - The selected prefix for the ID.
+ * @param {number} [options.length=8] - The length of the generated part of the ID (excluding the prefix).
+ * @param {"all" | "numbers" | "letters"} [options.charactersSet="all"] - The character set to use.
+ * @param {string} [options.includeChars] - Additional characters to include in the character set.
+ * @param {string} [options.excludeChars] - Characters to exclude from the character set.
+ * @returns {string} The generated ID. The total length of the ID will be equal to the length of the prefix plus the specified length.
  * @example
  * // Example 1: Using default parameters
- * // Defaults Parameters: { prefix: "JJPV", length: "8" }
+ * // Defaults Parameters: { prefix: "JJPV", length: 8 }
  * const generatedId = mid();
  * console.log(generatedId); // Outputs something like 'JJPVoVnm'
  *
  * // Example 2: Using custom parameters
- * const customId = mid({ prefix: "PVOC", length: "16", charactersSet: "all" });
+ * const customId = mid({ prefix: "PVOC", length: 16, charactersSet: "all" });
  * console.log(customId); // Outputs something like 'PVOC1hgJks7d5jT2'
  */
 export function mid({
-  prefix = "JJPV",
-  length = "8",
+  prefix = null,
+  length = 8,
   charactersSet = "all",
   includeChars,
   excludeChars,
 }: Options = {}): string {
+  if (length > 64) {
+    console.warn(
+      chalk.yellow(
+        "The specified length exceeds the maximum allowed length of 64. Setting length to 64."
+      )
+    );
+    length = 64;
+  } else if (length < 4) {
+    console.warn(
+      chalk.yellow(
+        "The specified length is less than the minimum allowed length of 4. Setting length to 4."
+      )
+    );
+    length = 4;
+  }
+
   let characters;
   switch (charactersSet) {
     case "all":
@@ -37,24 +59,22 @@ export function mid({
       characters = charactersAll;
   }
 
-  // Include specific characters if provided
   if (includeChars) {
     characters += includeChars;
   }
 
-  // Exclude specific characters if provided
   if (excludeChars) {
     characters = characters.replace(new RegExp(`[${excludeChars}]`, "g"), "");
   }
 
-  // Calculate how many characters we need to generate to complete the specified length
-  const remainingLength = Number(length) - prefix.length;
-
-  // Generate the remaining characters
-  let id = prefix;
-  for (let i = 0; i < remainingLength; i++) {
+  let id = "";
+  for (let i = 0; i < length; i++) {
     id += characters[Math.floor(Math.random() * characters.length)];
   }
 
-  return id;
+  const finalId = prefix ? prefix + id : id;
+
+  return finalId;
 }
+
+console.log(mid({ length: 130, charactersSet: "all" }));
--- src/models/type-prefix.ts
+++ src/models/type-prefix.ts
@@ -1,24 +1,6 @@
-export type Prefix =
-  | "PVOC"
-  | "0204"
-  | "JJLM"
-  | "1230"
-  | "JJPV"
-  | "0230"
-  | "FOLD"
-  | "INVE"
-  | "AUTH"
-  | "MART"
-  | "JEFF"
-  | "PROD"
-  | "OQUE"
-  | "CENT"
-  | "PAUL"
-  | "VALE";
-
 export interface Options {
-  prefix?: Prefix;
-  length?: "8" | "16" | "32" | "64";
+  prefix?: string | null;
+  length?: number;
   charactersSet?: "all" | "numbers" | "letters";
   includeChars?: string;
   excludeChars?: string;
